97445dc2f14b5ab15a410509825a8f7a06784102
Nick <nicholas.pullara@gmail.com>
2023-08-07 15:48:43 -0400

    >created genB, kruth_b random generator in gameController class.

--- engine.h
+++ engine.h
@@ -3,6 +3,8 @@
 // Game Engine Prototype
 
 #include <fstream>
+#include <random>
+
 
 #ifndef PROTOTYPEMECHCOMBAT_ENGINE_H
 #define PROTOTYPEMECHCOMBAT_ENGINE_H
@@ -14,6 +16,10 @@ namespace eng {
         int turnCount = 0;
 
 
+
+        std::random_device rd;
+        unsigned seed;
+
     public:
 
         //Setup game Parameters including player name
@@ -24,19 +30,27 @@ namespace eng {
          */
         gameController()= default;
         bool init() {
+
+
+            //Seed random number generator with true random value
+            std::knuth_b genB (this->seed);
+            std::cout << "\nRandom Number: " << genB() << std::endl;
+
             std::ofstream *saveFile = new std::ofstream;
             std::cout << "Enter your name (Must be perfect to save correctly)" << " :";
             std::cin
                     >> this->plyName;                                                //This must be changed to allow save imp.
-            saveFile.open(plyName.append(".txt"));                              //Open the playername.txt file
+            saveFile->open(plyName.append(".txt"));                              //Open the playername.txt file
             //Read until end of file
             //Write date and time on new line
-            saveFile.close();
+            saveFile->close();
             this->turnCount = 0;
             return true;
         }
 
+        bool loop(){
 
+        }
     };
 
 
